a44a64d153c5a35230acc2d02192135472b8ebf1
M4rcoos <contato.shopvmax@gmail.com>
2023-10-10 22:38:39 -0300

    feat: initing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import  * as C from "./AppStyles.tsx"
 
 function App() {
 
   return (
-    <>
-    <h1>olá mundo</h1>
-    </>
+    <C.Container>
+    <C.Content>
+      <C.Header>
+      <img src="src/assets/foguinho.png" width={130} height={130} alt="foguinho" />
+      <div style={{marginBottom:-45}}>
+      <img src="src/assets/raio.png" width={160} height={180} alt="raio" />
+      </div>
+      </C.Header>
+      <C.Title>
+      Estude a bíblia de maneira simples e divertida
+      </C.Title>
+      <C.Footer>
+
+      <img src="src/assets/tres-coracoes.png" width={210} height={145}alt="tres corações" />
+      <img src="src/assets/biblia.png"  width={180} height={170} alt="icone de uma biblia" />
+      <img src="src/assets/createdBy.png"  width={300} height={200}alt="ticket" />
+      </C.Footer>
+    </C.Content>
+    </C.Container>
   )
 }
 
---
+++ src/AppStyles.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+export const Content = styled.main`
+ display:flex;
+ flex-direction:column;
+ align-items:center;
+ justify-content:center;
+ max-width: 64.375rem;
+`
+export const Header = styled.div`
+ display:flex;
+ align-items:center;
+ justify-content:space-between;
+ width:100%;
+ margin-bottom: -100px;
+
+`
+export const Footer = styled.div`
+display:flex;
+align-items:center;
+justify-content:space-between;
+width:100%;
+margin-top: -45px;
+
+`
+
+export const Title = styled.h1`
+font-family: 'Poppins', sans-serif;
+font-size:xx-large;
+font-size: 5.6rem;
+text-align: center;
+color:#000;
+`
