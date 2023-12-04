19cfae1197536501eb9167e03325a495f988f2eb
Samuel <samuel.abreu2511@gnmail.com>
2023-12-04 18:53:37 -0300

    Subindo alterações

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ export const Profile = () => {
       <section className='profile' id='profile'>
 
         <div className='contenti-mage' >
-        <img src={profile} alt=""
+        <img src={profile} alt="imagem do perfil"
           />
         </div>
      
